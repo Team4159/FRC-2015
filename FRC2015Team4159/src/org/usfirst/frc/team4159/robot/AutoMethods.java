312bdd3de3799b90e434282c80a12883689d6605
sageshaw <seiji.shaw@gmail.com>
2015-02-13 18:01:44 -0800

    added sendable chooser

--- FRC2015Team4159/src/org/usfirst/frc/team4159/robot/AutoMethods.java
+++ FRC2015Team4159/src/org/usfirst/frc/team4159/robot/AutoMethods.java
@@ -1,16 +1,35 @@
 package org.usfirst.frc.team4159.robot;
 
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 public class AutoMethods {
 	private NetworkTable table;
 	private OctoDrive mainDrive;
 	private ToteLifter mainElevator;
 	
+	private static final Integer MOVE_ONLY = 1;
+	private static final Integer PICK_ONE_TOTE = 2;
+	private static final Integer PICK_TWO_TOTE_SKIP = 3;
+	private static final Integer PICK_TWO_TOTE_NOSKIP = 4;
+	private static final Integer PICK_THREE_TOTE = 5;
+	private static final Integer GTFO_TOTE = 6;
+	
+	
+	SendableChooser autoChooser;
+	
 	public AutoMethods(OctoDrive mainDriveTemp, ToteLifter Elevator) {
 		table = NetworkTable.getTable("");
 		mainDrive = mainDriveTemp;
 		mainElevator = Elevator;
+		
+		autoChooser = new SendableChooser();
+		autoChooser.addDefault("Move Only", MOVE_ONLY);
+		autoChooser.addObject("1 Tote Pickup", PICK_ONE_TOTE);
+		autoChooser.addObject("2 Tote Pickup(Skip)", PICK_TWO_TOTE_SKIP);
+		autoChooser.addObject("2 Tote Pickup(No skip)", PICK_TWO_TOTE_NOSKIP);
+		autoChooser.addObject("3 Tote Pickup", PICK_THREE_TOTE);
+		autoChooser.addObject("Straight Shot 3 Tote Pickup", GTFO_TOTE);
 	}
 	
 	public void AutoSelected(){
@@ -25,4 +44,5 @@ public class AutoMethods {
 			table.retrieveValue("XOffset", xOffSet);
 		}
 	}
+	
 }
