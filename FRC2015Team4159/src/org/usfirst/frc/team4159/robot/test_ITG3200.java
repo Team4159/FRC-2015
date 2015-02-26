47ade45f6fdc234e92703f3ce9e5a917071e209f
Ethan Durrant <emd519@yahoo.com>
2015-02-25 17:26:52 -0800

    starting i2c gryo stuff

---
+++ FRC2015Team4159/src/org/usfirst/frc/team4159/robot/test_ITG3200.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4159.robot;
+
+import edu.wpi.first.wpilibj.I2CJNI;
+
+public class test_ITG3200 {
+	
+	private byte itg_addr;
+	
+	public void test_ITG3200(byte address){
+		itg_addr = address;
+		
+	}
+}
