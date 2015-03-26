24ec564931681602a4b02e1f5732b677f34046b9
sageshaw <seiji.shaw@gmail.com>
2015-03-26 16:20:16 -0700

    added dynamic speed control to auto-balancing

--- FRC2015Team4159/src/org/usfirst/frc/team4159/robot/AutoBalancer.java
+++ FRC2015Team4159/src/org/usfirst/frc/team4159/robot/AutoBalancer.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class AutoBalancer {
 	private static final double OFFBALANCEANGLE = 40;       //Tune for actual robot (during SVR)
 	private static final double ONBALANCEANGLE = 10;
+	private static double angle_in_radians;
+	private static double driveMagnitude;
 	
 	private AutoBalancer() {
 		
@@ -13,15 +15,13 @@ public class AutoBalancer {
 	public static void balanceFeed(float angle) {
 		while (angle >= OFFBALANCEANGLE && angle > ONBALANCEANGLE) {
 			SmartDashboard.putBoolean("Auto-Balance_Engaged", true);
-			OctoDrive.autoDrive.drive(-0.5, 0.0);
+			angle_in_radians = Math.toRadians(angle);
+			driveMagnitude = -1 * Math.sin(angle_in_radians);
+			
+			OctoDrive.autoDrive.drive(driveMagnitude, 0.0);
 			angle = IO.imu.getPitch();
 		}
-		while (angle <= -OFFBALANCEANGLE && angle < -ONBALANCEANGLE) {
-			SmartDashboard.putBoolean("Auto-Balance_Engaged", true);
-			OctoDrive.autoDrive.drive(0.5, 0.0);
-			angle = IO.imu.getPitch();
-		}
-		
+
 		SmartDashboard.putBoolean("Auto-Balance_Engaged", false);
 	}
 	
