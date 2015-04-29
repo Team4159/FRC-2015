40a4264b1259fcf3e2ebe09cb214f46534e1503b
sageshaw <seiji.shaw@gmail.com>
2015-04-29 07:03:02 -0700

    Added Primary Driver's Control Over Elevator

--- FRC2015Team4159/src/org/usfirst/frc/team4159/robot/Robot.java
+++ FRC2015Team4159/src/org/usfirst/frc/team4159/robot/Robot.java
@@ -165,9 +165,9 @@ public class Robot extends IterativeRobot {
 			ifSevenDown = false;
 		}
 
-		if (IO.secondaryStick.getRawButton(3)) { // Moves elevator up
+		if (IO.secondaryStick.getRawButton(3) || IO.rightStick.getRawButton(3)) { // Moves elevator up
 			IO.elevator.autoLift(elevatorValue);
-		} else if (IO.secondaryStick.getRawButton(2)) { // Moves elevator down
+		} else if (IO.secondaryStick.getRawButton(2) || IO.rightStick.getRawButton(2)) { // Moves elevator down
 			IO.elevator.autoLift(-elevatorValue);
 		} else {
 			IO.elevator.autoLift(0.0); // Stops elevator if there is no joystick
